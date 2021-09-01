fb340a637d03efc59d306eb1e820ac91eea55c5d
Yongxin Tan <yt150@duke.edu>
2021-09-01 19:14:43 +0000

    added logic to get the most frequent character in a string

--- main.py
+++ main.py
@@ -7,12 +7,24 @@ app = FastAPI()
 async def root():
     return {"message": "Hello Duke"}
 
-@app.get("/add/{num1}/{num2}")
-async def add(num1: int, num2: int):
-    """Add two numbers together"""
+@app.get("/StrMostFreq/{input_str}")
+async def add(input_str: str):
+    """Find the most frequent character in a string"""
 
-    total = num1 + num2
-    return {"total": total}
+    freq = {}
+    for i in input_str:
+        if i in freq:
+            freq[i] += 1
+        else:
+            freq[i] = 1
+    
+    max_key = max(zip(freq.values(), freq.keys()))[1]
+    max_val = max(freq.values()) 
+    
+    return {
+        "most_frequent_key": max_key,
+        "frequency": max_val
+    }
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
